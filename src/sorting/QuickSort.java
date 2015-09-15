c7200ab725dcfeaea15aba24303534d1b52ecea0
Breezee Wu <wasteutime@126.com>
2015-09-15 23:37:52 +0800

    QuickSort and Sortable

---
+++ src/sorting/QuickSort.java
@@ -0,0 +1,49 @@
+package sorting;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * Created by Hongze Wu on 2015/9/15.
+ */
+public class QuickSort<T extends Sortable> {
+    private int size;
+    private T[] buffer;
+
+    public QuickSort(int size) {
+        this.size = size;
+        buffer = (T[])new Object[size];
+    }
+
+    public void recursiveQuickSort(int p, int r) {
+        if(p < r) {
+            int q = partition(p, r);
+            recursiveQuickSort(p, q-1);
+            recursiveQuickSort(q+1, r);
+        }
+    }
+
+    public int partition(int p, int r) {
+        T pivot = buffer[r];
+        int i = p - 1;
+
+        for(int j = p; j <= (r-1); j++) {
+            //if(buffer[j] <= pivot) {
+            if(buffer[j].compareTo(pivot) <= 0) {
+                i++;
+
+                // swap
+                T tmp = buffer[i];
+                buffer[i] = buffer[j];
+                buffer[j] = tmp;
+            }
+        }
+
+        // swap i+1 with pivot
+        T tmp = buffer[i+1];
+        buffer[i+1] = buffer[r];
+        buffer[r] = tmp;
+
+        return i+1;
+    }
+}
---
+++ src/sorting/Sortable.java
@@ -0,0 +1,17 @@
+package sorting;
+
+/**
+ * Created by Hongze Wu on 2015/9/15.
+ */
+public interface Sortable<T> {
+    /*
+     * @param   o the object to be compared.
+     * @return  a negative integer, zero, or a positive integer as this object
+     *          is less than, equal to, or greater than the specified object.
+     *
+     * @throws NullPointerException if the specified object is null
+     * @throws ClassCastException if the specified object's type prevents it
+     *         from being compared to this object.
+     */
+    public int compareTo(T o);
+}
